07272256a5eecba0855f03583ced1e2f701772db
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2024-05-24 20:40:30 +0300

    add product controller and schema

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,18 @@ import {
 } from './controllers/session.controller';
 import { createSessionSchema } from './schema/session.schema';
 import requireUser from './middleware/requireUser';
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from './schema/product.schema';
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from './controllers/product.controller';
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
@@ -23,6 +35,30 @@ function routes(app: Express) {
 
   app.get('/api/sessions', requireUser, getUserSessionsHandler);
   app.delete('/api/sessions', requireUser, deleteSessionHandler);
+
+  app.post(
+    '/api/products',
+    [requireUser, validate(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    '/api/products/:productId',
+    [requireUser, validate(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    '/api/products/:productId',
+    validate(getProductSchema),
+    getProductHandler
+  );
+
+  app.delete(
+    '/api/products/:productId',
+    [requireUser, validate(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;
